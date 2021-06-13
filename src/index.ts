ca45341c0a34e9fc6d29ea044217f32f5599e690
John Chipps-Harding <john.chipps-harding@phntms.com>
2021-06-13 17:59:47 +0100

    First Draft

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
-interface Props {
-  argument1: number;
-  argument2?: number;
+import auth, { BasicAuthResult } from "basic-auth";
+import { DocumentContext } from "next/document";
+import compare from "tsscmp";
+
+interface MiddlewareConfig {
+  name: string;
+  pass: string;
+  realm: string;
+  message: string;
 }
 
-const useLibrary = ({ argument1, argument2 = 0 }: Props) => {
-  if (!argument1) throw Error("argument 1 not specified");
-  return {
-    something: argument1 + argument2,
-  };
+export const baseConfig: MiddlewareConfig = {
+  name: "admin",
+  pass: "password",
+  realm: "site",
+  message: "401 Access Denied",
 };
 
-export default useLibrary;
+const basicAuthMiddleware = async (
+  { req, res }: DocumentContext,
+  config?: Partial<MiddlewareConfig>
+) => {
+  if (!req || !res || !res.end) return;
+
+  const conf = { ...baseConfig, ...config };
+
+  const currentUser = auth(req);
+  if (!currentUser || !check(currentUser, conf)) {
+    res.statusCode = 401;
+    res.setHeader("WWW-Authenticate", `Basic realm="${conf.realm}"`);
+    res.end(conf.message);
+  }
+};
+
+const check = (currentUser: BasicAuthResult, config: MiddlewareConfig) =>
+  compare(currentUser.name, config.name) &&
+  compare(currentUser.pass, config.pass);
+
+export default basicAuthMiddleware;
